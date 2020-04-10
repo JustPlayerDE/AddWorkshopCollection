164170a75e40530058b250fcf991c19baa9ab9df
Justin Klüver <justin.k@justplayer.de>
2020-04-10 17:42:16 +0200

    Path can now be set in another file (ignored by git)

--- update.bat
+++ update.bat
@@ -1,7 +1,6 @@
 @echo off
 set WSID=2055027639
-set GPath=..\..\..\bin
-
+set /p GPath=<path.txt
 %GPath%\gmad.exe create -out .\gma.gma -warninvalid -folder "%~dp0
 %GPath%\gmpublish.exe update -id %WSID% -addon .\gma.gma
 %GPath%\gmpublish.exe update -id %WSID% -icon "%~dp0/icon.jpg"

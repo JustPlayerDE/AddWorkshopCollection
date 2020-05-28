6f851ff7c103e1c9d44a3048dabd91ab6eb56ea7
Justin Klüver <justin.k@justplayer.de>
2020-05-28 03:57:50 +0200

    Recoded to use the API

--- lua/autorun/server/justplayer_workshopdownloader.lua
+++ lua/autorun/server/justplayer_workshopdownloader.lua
@@ -5,81 +5,100 @@
  Thanks to Tom.bat for pointing out that there is actually an API 
 ]]
 local CollectionQueue = {} -- All collections are put here to wait for SteamHTTP starting up
+local ADDON, COLLECTION = 0, 2
 
-local function parseAddons(source)
-    local addons = {}
+function resource.AddWorkshopCollection(collectionIds, _recursiveprotection)
+    if collectionIds == nil then return end
+    _recursiveprotection = _recursiveprotection or {}
+    local _Collections = {}
+    local Collections = {}
+    local Addons = {}
 
-    -- Parsing Collection Page
-    for s in source:gmatch("SubscribeItemBtn%d+") do
-        local id = s:gsub("SubscribeItemBtn", "")
-        table.insert(addons, id)
+    if isstring(collectionIds) then
+        collectionIds = tonumber(collectionIds)
     end
 
-    return addons
-end
+    if isnumber(collectionIds) then
+        table.insert(_Collections, collectionIds)
+    else
+        _Collections = collectionIds
+    end
 
-local function parseCollections(source)
-    local source_ = string.Explode("<div class=\"collectionChildren\">", source)[3] or ""
-    local collections = {}
+    if #_Collections <= 0 then return end
 
-    for s in source_:gmatch("id=[%d]+") do
-        local id = s:gsub("id=", "")
+    local POST = {
+        ["collectioncount"] = tostring(#_Collections)
+    }
 
-        if not table.HasValue(collections, id) then
-            table.insert(collections, id)
+    for i = 0, #_Collections - 1 do
+        local ID = _Collections[i + 1]
+        -- We dont want invalid Workshop IDs here
+        assert(tonumber(ID), "Invalid Workshop Collection id: " .. ID)
+        POST["publishedfileids[" .. i .. "]"] = tostring(ID)
+    end
+
+    http.Post("https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1/", POST, function(body, _, _, status)
+        local json = util.JSONToTable(body)
+
+        if status ~= 200 or not (json and json.response) then
+            PrintTable(POST)
+            error("Error while fetching collection data. Status: " .. status)
         end
-    end
 
-    return collections
-end
+        local CollectionData = json.response.collectiondetails or {}
 
-function resource.AddWorkshopCollection(collectionid, _recursive)
-    if collectionid == nil then return end
-    _recursive = _recursive or {}
+        -- Going trough all Collection items
+        for _, collection in ipairs(CollectionData) do
+            local Items = collection.children or {}
 
-    if CollectionQueue ~= nil then
-        table.insert(CollectionQueue, collectionid)
-        print("[WORKSHOP] Queued Collection #" .. collectionid .. " for later.")
+            -- Adding them to either Collection or Addon table
+            for i = 1, #Items do
+                local Item = Items[i]
 
-        return
-    end
+                if Item.filetype == ADDON then
+                    table.insert(Addons, Item.publishedfileid)
+                end
 
-    if table.HasValue(_recursive, collectionid) then return end
-    table.insert(_recursive, collectionid)
+                if Item.filetype == COLLECTION then
+                    -- Prevent adding already added collections
+                    if table.HasValue(_recursiveprotection, Item.publishedfileid) then return end
+                    table.insert(_recursiveprotection, Item.publishedfileid)
+                    -- Otherwise add them to the collection list
+                    table.insert(Collections, Item.publishedfileid)
+                end
+            end
+        end
 
-    -- Fetching Collection page
-    http.Fetch("http://steamcommunity.com/sharedfiles/filedetails/?id=" .. collectionid, function(source)
-        local addons = parseAddons(source)
-
-        -- Clientside "Mounting" of Collection items
-        for k, id in ipairs(addons) do
+        -- Adding Workshop addons
+        for k, id in ipairs(Addons) do
             resource.AddWorkshop(id)
         end
 
-        if #addons > 0 then
-            print("[WORKSHOP] Added Collection " .. collectionid .. " (" .. #addons .. " Addons) to Downloads.")
+        if #Addons > 0 then
+            print("[WORKSHOP] Added " .. #_Collections .. " Collection(s) (Total " .. #Addons .. " Addons) to Downloads.")
         end
 
+        -- Recursive AddWorkshopCollection
         if GetConVar("sv_addworkshopcollection_recursive"):GetBool() then
-            local collections = parseCollections(source)
-
-            for i, k in ipairs(collections) do
-                resource.AddWorkshopCollection(k, _recursive)
+            for i, collectionid in ipairs(Collections) do
+                resource.AddWorkshopCollection(collectionid, _recursiveprotection)
             end
         end
-    end, function()
-        error("Error while fetching collection #" .. collectionid)
+    end, function(error)
+        error("Error while fetching collection(s) " .. table.concat(_Collections, ",") .. " Error: " .. error)
 
         return false
     end)
 end
 
+print("============")
+
+--resource.AddWorkshopCollection(1754190594)
+--resource.AddWorkshopCollection({1754190594 , 1915277996})
 -- usage: sv_addworkshopcollection <collection ids>
 -- Example: sv_addworkshopcollection 1 2 3 4 (...)
 concommand.Add("sv_addworkshopcollection", function(_, _, args)
-    for k, id in ipairs(args) do
-        resource.AddWorkshopCollection(id)
-    end
+    resource.AddWorkshopCollection(args)
 end)
 
 CreateConVar("sv_addworkshopcollection_recursive", 0, FCVAR_ARCHIVE, "Enables Recursive addWorkshopCollection")
@@ -90,8 +109,5 @@ hook.Add("Think", "AddWorkShopCollection:RunQueued", function()
     print("[WORKSHOP] Running AddWorkshopCollection Queue..")
     local queue = CollectionQueue
     CollectionQueue = nil -- Disable Queue
-
-    for k, id in ipairs(queue) do
-        resource.AddWorkshopCollection(id)
-    end
+    resource.AddWorkshopCollection(queue)
 end)
